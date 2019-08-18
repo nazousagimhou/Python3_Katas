ccbb57923c5bb4aa2c7197d69a5be0f2e1846f5b
ale_tovar <tovrale3ds@gmail.com>
2019-08-18 14:57:58 -0500

    Se restauró la función

--- QuitarVocales.py
+++ QuitarVocales.py
@@ -1,4 +1,5 @@
 #  Este codigo es otra solucion
-return string.replace("A","").replace("a","").replace("E","").replace("e","").replace("I","").replace("i","").replace("O","").replace("o","").replace("U","").replace("u","")
+def disemvowel(string):
+	return string.replace("A","").replace("a","").replace("E","").replace("e","").replace("I","").replace("i","").replace("O","").replace("o","").replace("U","").replace("u","")
 
-print(disemvowel(""))   
+print(disemvowel("Hola sin con ummm"))
